3141d7589b7378b6f4104b6a20946bcf8a63f760
robert baxter <baxterrp@gmail.com>
2026-06-06 09:58:40 -0400

    added json formatting

--- src/repostat/main.py
+++ src/repostat/main.py
@@ -1,7 +1,8 @@
 from typing import Any
+from rich import print_json
 
+import json
 import httpx
-
 
 def get_repo(owner: str, repo_name: str) -> dict[str, Any]:
     url = f"https://api.github.com/repos/{owner}/{repo_name}"
@@ -9,22 +10,20 @@ def get_repo(owner: str, repo_name: str) -> dict[str, Any]:
     response.raise_for_status()
     return response.json()
 
-
 def main():
     owner = "baxterrp"
     repo_name = "repostat"
     repo_info = get_repo(owner, repo_name)
     print_repo_info(repo_info)
 
-
 def print_repo_info(repo_info: dict[str, Any]) -> None:
-    delimiter = ", "
+    topic_delimeter = ", "
+    print(f"response: {print_json(json.dumps(repo_info))}")
     print(f"repository: {repo_info['full_name']}")
     print(f"description: {repo_info['description']}")
     print(f"stars: {repo_info['stargazers_count']}")
     print(f"default branch: {repo_info['default_branch']}")
-    print(f"topics: {delimiter.join(repo_info.get('topics') or [])}")
-
+    print(f"topics: {topic_delimeter.join(repo_info.get('topics') or [])}")
 
 if __name__ == "__main__":
     main()
